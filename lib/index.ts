74df8da273ce31af0cb29f90a3d7a1ef17c33072
赵思阳 <zhaosiyang@s-MacBook-puro.local>
2018-10-08 01:44:44 -0400

    build dist

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,7 @@
 import * as memoizee from 'memoizee';
 
 export function memoize(options?: memoizee.Options) {
-  return function(target, key, descriptor) {
+  return function(target: any, key: PropertyKey, descriptor: PropertyDescriptor) {
     const oldFunction = descriptor.value;
     const newFunction = memoizee(oldFunction, options);
     descriptor.value = function () {
